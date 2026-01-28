d1ee5d6cc0ad56903ded400c9b4c8823c9f3dc2f
CrazM13 <sepulvedajoey13@yahoo.com>
2026-01-27 19:59:37 -0500

    Added toggle to gate

--- Scripts/Gate.cs
+++ Scripts/Gate.cs
@@ -6,6 +6,7 @@ public partial class Gate : Node2D {
 	[Export] private string keyID;
 	[Export] private StaticBody2D collision;
 	[Export] private AnimatedSprite2D sprite;
+	[Export] private bool perminantUnlock = false;
 
 	private uint collisionData;
 
@@ -20,7 +21,7 @@ public partial class Gate : Node2D {
 		if (key.id == keyID) {
 			Open();
 		} else {
-			Close();
+			if (!perminantUnlock) Close();
 		}
 	}
 
