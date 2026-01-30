5920b6a37a27eaf493a4d41e70648aa179738f20
CrazM13 <sepulvedajoey13@yahoo.com>
2026-01-30 18:22:55 -0500

    Fixed lighting issues

--- Scripts/SceneManagement/LoadLevel.cs
+++ Scripts/SceneManagement/LoadLevel.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class LoadLevel : Node {
 
-	public static string levelToLoad = "res://Scenes/Level1.tscn";
+	public static string levelToLoad = "res://Scenes/Level2.tscn";
 
 	public override void _Ready() {
 		base._Ready();
